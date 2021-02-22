21d6424dbf93aadf5999815674ee269586391e3b
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-22 16:12:33 -0500

    played around with a few functions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,25 @@
+// import React, { useState } from "react";
+
 import React, { useState } from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0);
 
-  function increment() {
-    setCount(count + 1);
-  }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+function Counter(){
+  const [count, setCount] = useState(0)
+
+    function increment(){
+      // console.log(`Before setState: ${count}`)
+      // setCount(count + 1)
+      // setCount(count + 1)
+      // console.log(`After setState: ${count}`)
+
+      setCount((currentCount) => currentCount + 1)
+      setCount((currentCount) => currentCount + 1)
+     
+    }
+
+  return <button onClick={increment}>{count}</button>
+
 }
 
 export default Counter;
